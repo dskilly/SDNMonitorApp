0ecb091ea0355f126b57fed810585bd8999d3d42
dskilly <nskilly@gmail.com>
2019-03-26 03:21:17 -0400

    Made changes to Appmonitor app to use new models and integrate the models in to the site template

--- Appmonitor/models.py
+++ Appmonitor/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class port_stats(models.Model):
+	device_id = models.TextField()
+	port_id = models.IntegerField(primary_key=True)
+	rx_packets = models.IntegerField()
+	tx_packets = models.IntegerField()
+	rx_dropped = models.IntegerField()
+	tx_dropped = models.IntegerField()
+	rx_errors = models.IntegerField()
+	tx_errors = models.IntegerField()
+
+class flow_stats(models.Model):
+	length = models.IntegerField()
+	table_id = models.TextField()
+	duration_sec = models.IntegerField()
+	priority = models.IntegerField()
+	idle_timeout = models.IntegerField()
+	hard_timeout = models.IntegerField()
+	packet_count = models.IntegerField()
+	
--- Appmonitor/views.py
+++ Appmonitor/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import port_stats, flow_stats
 
 
 # Create your views here.
 
 def Appmonitorsite(request):
-	return render(request,'Appmonitor/Appmonitorsite.html')
+	devices = port_stats.objects.values('device_id').distinct()
+	nodes = [{name: x.device_id, tx_errors: sum([y.tx_errors for y in port_stats.objects.filter(device_id=x.device_id)])} for x in nodes]
+	return render(request,'Appmonitor/Appmonitorsite.html', {'nodes': nodes, 'flows': flow_stats.objects.all()})
