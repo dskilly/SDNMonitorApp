0cf45e4ad06a7fd620fce6bf33929c8db3006e47
Nisson Nie Chen <nskilly@gmail.com>
2019-03-28 11:46:09 -0400

    Fixed minor syntax inconsistencies

--- Appmonitor/views.py
+++ Appmonitor/views.py
@@ -10,6 +10,6 @@ def Appmonitorsite(request):
 	devices = port_stats.objects.values('device_id').distinct()
 	nodes = []
 	for device in devices:
-		name = device['device_id']
+		name = device.device_id
 		nodes.append({'name': name, 'tx_errors': sum([x.tx_errors for x in port_stats.objects.filter(device_id=name)])})
 	return render(request,'Appmonitor/Appmonitorsite.html', {'nodes': nodes, 'flows': flow_stats.objects.all()})
