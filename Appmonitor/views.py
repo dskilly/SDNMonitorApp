8ea91692772727e7a71382f8062ec62109920d57
Nisson Nie Chen <nskilly@gmail.com>
2019-03-28 04:15:19 -0400

    Column chart implemented and working, missing integration for the flow table

--- Appmonitor/views.py
+++ Appmonitor/views.py
@@ -8,5 +8,8 @@ from .models import port_stats, flow_stats
 
 def Appmonitorsite(request):
 	devices = port_stats.objects.values('device_id').distinct()
-	nodes = [{name: x.device_id, tx_errors: sum([y.tx_errors for y in port_stats.objects.filter(device_id=x.device_id)])} for x in nodes]
+	nodes = []
+	for device in devices:
+		name = device['device_id']
+		nodes.append({'name': name, 'tx_errors': sum([x.tx_errors for x in port_stats.objects.filter(device_id=name)])})
 	return render(request,'Appmonitor/Appmonitorsite.html', {'nodes': nodes, 'flows': flow_stats.objects.all()})
